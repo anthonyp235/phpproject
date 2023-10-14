67e380cf91f526f41d2708317ec681c889417ca0
Anthony <anthonyp235@hotmail.com>
2023-10-14 19:51:14 -0400

    First commit

---
+++ database.php
@@ -0,0 +1,41 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "phpassigment";
+
+// Here we insert the data the user sent us
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $conn = new mysqli($servername, $username, $password, $database);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $email = $conn->real_escape_string($_POST['email']);
+    $interests = implode(', ', $_POST['interests']);  // Combine interests into a comma-separated string
+
+    $sql = "INSERT INTO subscribers (email, interests) VALUES ('$email', '$interests')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Record inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+
+}
+$result = $conn->query("SELECT email, interests FROM subscribers");
+if ($result->num_rows > 0) {
+    echo "<h2>All our members:</h2>";
+    echo "<ul>";
+    while($row = $result->fetch_assoc()) {
+        echo "<li>Email: " . $row["email"]. ", Interests: " . $row["interests"]. "</li>";
+    }
+    echo "</ul>";
+} else {
+    echo "The list is empty";
+}
+
+$conn->close();
+// Here we have the code to display old records
+?>
+
